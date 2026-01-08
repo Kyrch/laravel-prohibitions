bc1312f758fccabd6d84485db7293d16f98f350f
Kyrch <kyrchk5@gmail.com>
2026-01-08 20:02:59 -0300

    docs: add docs to config file

--- config/prohibition.php
+++ config/prohibition.php
@@ -9,8 +9,35 @@ use Kyrch\Prohibition\Pivots\ModelProhibition;
 use Kyrch\Prohibition\Pivots\ModelSanction;
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Events Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Enables or disables all package-related events.
+    |
+    | When set to true, the package will dispatch events during actions such as:
+    | - ModelProhibitionTriggered
+    | - ModelSanctionTriggered
+    |
+    | Disable this if you want full manual control or to avoid side effects
+    | in specific environments (e.g. testing).
+    |
+    */
     'events_enabled' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Model Classes
+    |--------------------------------------------------------------------------
+    |
+    | These values define the Eloquent model classes used internally
+    | by the package.
+    |
+    | All models must extend its respective model.
+    | You may override any of these to use your own custom implementations.
+    |
+    */
     'models' => [
         'user' => User::class,
         'prohibition' => Prohibition::class,
@@ -19,6 +46,17 @@ return [
         'model_sanction' => ModelSanction::class,
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Table Names
+    |--------------------------------------------------------------------------
+    |
+    | Defines the database table names used by the package.
+    |
+    | You may change these if your project uses a different naming
+    | convention or if table names conflict with existing tables.
+    |
+    */
     'table_names' => [
         'prohibition' => 'prohibitions',
         'sanction' => 'sanctions',
